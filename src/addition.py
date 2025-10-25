73792a135d0e2b360533ce86945eb5cbd0186c1d
Ubuntu <ubuntu@ip-172-31-2-159.ap-south-1.compute.internal>
2025-10-25 07:27:24 +0000

    added subtract funtion

--- src/addition.py
+++ src/addition.py
@@ -3,6 +3,16 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    assert sub(10, 5) == 5
+    assert sub(15, 5) == 10
+
+
+test_add()
+
+print("Hello World")
